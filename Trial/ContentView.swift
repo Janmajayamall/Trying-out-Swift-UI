1a9e2dbdbeb4f0fcfb49de0a16cad8791bf78815
Janamajaya Mall <janmajaya.choithram@gmail.com>
2020-07-16 07:31:22 +0530

    Initial Commit

---
+++ Trial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trial
+//
+//  Created by Janmajaya Mall on 16/7/2020.
+//  Copyright © 2020 Janmajaya Mall. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
